311401c0d7de392526c7cfacf7ec1b9ce8035fa1
Your Name <ren.khizbullin@gmail.com>
2022-05-30 20:42:40 +0300

    run

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', required=True, type=str, help='Path to config file')
-    args = parser.parse_known_args()
+    parser.add_argument('config', required=True, type=str, help='Path to config file')
+    args, _ = parser.parse_known_args()
 
     if not os.path.exists(args.config):
         raise Exception(f"Config file {args.config} is not exists")
